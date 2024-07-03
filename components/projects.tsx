5554c5a6c28ee3c3fa5eab8d89fd4b564f4af5cb
Navnoor Singh Bal <navnoorsinghbal@Brainwaves-MacBook-Pro-2.local>
2024-07-03 12:37:45 +0530

    HTML ico bug

--- components/projects.tsx
+++ components/projects.tsx
@@ -33,7 +33,7 @@ const projects = [
     img: "/Projects/4.png",
     des: "The Common Gateway Interface (CGI) is a standard that facilitates communication between web servers and external databases or information sources. In this I have created a submission form which takes your information and one picture and then using CGI with Perl, it shows next page. I have create form in HTML with the help of Bootstrap. For php calling using POST method, I have utlized ajax in jquery, by which picture is saved on the server side, to be used by CGI to show client.",
     link: "https://github.com/navnoorsingh0309/Common_Gateway_Interface",
-    iconLists: ["/stack/HTML.png", "/stack/CSS.png", "/stack/php.png", "/stack/perl.png"]
+    iconLists: ["/stack/html.png", "/stack/CSS.png", "/stack/php.png", "/stack/perl.png"]
   },
   {
     id: "5",
