b595e9452e309ef83f4e1a9faaf1147f7afc98f3
navnoorsingh0309 <nbbal2003@gmail.com>
2025-01-22 14:06:17 +0530

    Many changes

--- components/experience.tsx
+++ components/experience.tsx
@@ -5,6 +5,8 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import Image from "next/image";
+import { Timeline } from "./ui/timeline";
 
 const skills = [
   {
@@ -166,6 +168,82 @@ const experiences = [
 ];
 
 const Experience = () => {
+
+  // Experience Data
+  const experiences1 = [
+    {
+      title: "Aug 2023 - May 2024",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Worked at iHub-AWaDH, IIT Ropar as a Firmware Intern
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/experience/awadh/1.jpeg"
+              alt="AWaDH"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/experience/awadh/2.jpeg"
+              alt="AWaDH"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Jan 2024 - Apr 2024",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Worked at IKSHAMA Centre, IIT Mandi as a Machine Learning Intern.
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/experience/chematico technologies/1.jpeg"
+              alt="Chematico Technologies"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "May 2024 - July 2024",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Worked at Chematico Technologies as a Software Developer Intern.
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+          <Image
+              src="/experience/ikshma/1.png"
+              alt="IKSHMA"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/experience/ikshma/2.png"
+              alt="IKSHMA"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+  ];
+
   return (
     <section
       className="flex flex-col justify-center space-y-4 items-center bg-blue-950/50 w-full p-4 px-8 lg:px-60 xl:px-72 pt-[200px]"
@@ -212,54 +290,7 @@ const Experience = () => {
           My Work &nbsp; <p className="text-[#ffd700]">Experience</p>
         </h1>
         <div className="mt-12 flex justify-center">
-          <VerticalTimeline>
-            {experiences.map((experience, index) => (
-              <VerticalTimelineElement
-                visible={true}
-                key={experience.company_name}
-                date={experience.date}
-                iconStyle={{ background: experience.iconBg }}
-                icon={
-                  <div className="flex justify-center items-center w-full h-full">
-                    <img
-                      src={experience.icon}
-                      alt={experience.company_name}
-                      className="w-[60%] h-[60%] object-contain"
-                    />
-                  </div>
-                }
-                contentStyle={{
-                  borderBottom: "8px",
-                  borderStyle: "solid",
-                  borderBottomColor: experience.iconBg,
-                  boxShadow: "none",
-                }}
-              >
-                <div>
-                  <h3 className="text-black text-xl font-poppins font-semibold">
-                    {experience.title}
-                  </h3>
-                  <p
-                    className="text-black font-medium text-base"
-                    style={{ margin: 0 }}
-                  >
-                    {experience.company_name}
-                  </p>
-                </div>
-
-                <ul className="my-5 list-disc ml-5 space-y-2 text-left">
-                  {experience.points.map((point, index) => (
-                    <li
-                      key={`experience-point-${index}`}
-                      className="text-black/50 font-normal pl-1 text-sm text-left"
-                    >
-                      {point}
-                    </li>
-                  ))}
-                </ul>
-              </VerticalTimelineElement>
-            ))}
-          </VerticalTimeline>
+        <Timeline data={experiences1} />
         </div>
       </div>
       <hr className="border-slate-200" />{" "}
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,93 +1,150 @@
 "use client";
 
-import { PinContainer } from "./3d-pin";
+import { HoverEffect } from "./ui/card-hover-effect";
 
 const projects = [
   {
-    id: "1",
-    title: "Risc-V Assembler",
-    img: "/Projects/1.png",
-    des: "This is the implementation of a simple assembler which assembler Risc-V Assembly Code to Machine Code. The assembler supports various Risc-V instructions including R-type, I-Type, S-Type, SB-Type, U-Type, UJ-Type.",
-    link: "https://github.com/navnoorsingh0309/Riscv_Assembler",
-    iconLists: ["/stack/C++.png"]
+    title: "Hovercraft",
+    image: "/Projects/Hovercraft.jpg",
+    description: "Bluetooth controller hovercraft.",
+    link: "/",
   },
   {
-    id: "2",
-    title: "5 stage MIP32 Processor",
-    img: "/Projects/2.png",
-    des: "This is the implementation of a processor based on MIP32 Instruction Set Architecture(ISA) which can perform basic arithmetic operations and logical operations and can perform conditional branching. This processor can store and read data from a bank of registers using pipelining.",
-    link: "https://github.com/navnoorsingh0309/MIP32_Pipelined_Processor",
-    iconLists: ["/stack/Verilog.png"]
+    title: "Gesture Controller Car",
+    image: "/Projects/gesture control.jpg",
+    description: "Car controlled using gesture of hand",
+    link: "/",
   },
   {
-    id: "3",
-    title: "BOAA Website",
-    img: "/Projects/3.jpeg",
-    des: "I independently designed and developed the website for the Board of Academic Affairs at IIT Ropar using HTML, CSS, and Vanilla JavaScript. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quia recusandae nostrum id dolorem labore ipsum accusamus sequi magnam, optio praesentium inventore sapiente minima consequatur delectus temporibus dignissimos. Similique doloribus est commodi!",
-    link: "https://iitrpr.ac.in/boaa",
-    iconLists: ["/stack/html.png", "/stack/CSS.png", "/stack/js.png"]
+    title: "VR Hand Glove",
+    image: "/Projects/VR hand glove.jpg",
+    description: "Virtual Reality hand glove using unity.",
+    link: "/",
   },
   {
-    id: "4",
-    title: "Comman Gateway Interface (CGI)",
-    img: "/Projects/4.png",
-    des: "The Common Gateway Interface (CGI) is a standard that facilitates communication between web servers and external databases or information sources. In this I have created a submission form which takes your information and one picture and then using CGI with Perl, it shows next page. I have create form in HTML with the help of Bootstrap. For php calling using POST method, I have utlized ajax in jquery, by which picture is saved on the server side, to be used by CGI to show client.",
-    link: "https://github.com/navnoorsingh0309/Common_Gateway_Interface",
-    iconLists: ["/stack/html.png", "/stack/CSS.png", "/stack/php.png", "/stack/perl.png"]
+    title: "Omni wheel",
+    image: "/Projects/omni.jpg",
+    description: "Omni Wheel using mecanum wheels.",
+    link: "/",
   },
   {
-    id: "5",
-    title: "Advanced Encryption Standard (AES)",
-    img: "/Projects/5.png",
-    des: "Advanced Encryption Standard (AES) is a specification for the encryption of electronic data established by the U.S National Institute of Standards and Technology (NIST) in 2001. This is a simple implementation of Advanced Encryption Standard(AES-128) to encrypt and decrypt 16 byte of sample data using C++.",
-    link: "https://github.com/navnoorsingh0309/Advanced_Encryption_Standard",
-    iconLists: ["/stack/C++.png"]
+    title: "Adelaar",
+    image: "/Projects/Adelaar.jpeg",
+    description: "RC Plane.",
+    link: "/",
   },
   {
-    id: "6",
-    title: "TCP Client Server",
-    img: "/Projects/6.png",
-    des: "A Simple Chat Room which uses TCP service to convey messages and uses MongoDB for login and Sign Up purpose. It also utlitizes multithreading to support multiple requests at the same time. Sign Up features is also there to add new users and when one user left the chat, it automatically get deleted from server broadcasting channel.",
-    link: "https://github.com/navnoorsingh0309/TCP_Client_Server",
-    iconLists: ["/stack/python.png", "/stack/mongo.jpg"]
+    title: "Fleet Management in ROS2 Humble",
+    image: "/Projects/bharatforge.png",
+    description: "BharatForge Submission in Inter IIT Tech 13.0.",
+    link: "/",
   },
   {
-    id: "7",
-    title: "Sentimental Analysis",
-    img: "/Projects/7.png",
-    des: "This project showcases a Sentimental Analysis Model that utilizes RNN and LSTM. The Model predicts your text is positive or negative along giving a score between 0 and 1. In this project first we clean data using various pre-processing techniques and then we create our model, structure is given in github.",
-    link: "https://github.com/navnoorsingh0309/Sentimental_Analysis_LSTM",
-    iconLists: ["/stack/python.png", "/stack/tensor-flow.png"]
+    title: "JLR Chiplet Challenge",
+    image: "/Projects/jlr.jpeg",
+    description: "JLR Submission in Inter IIT Tech 12.0.",
+    link: "/",
   },
   {
-    id: "8",
-    title: "Gesture drawing using OpenCV",
-    img: "/Projects/8.png",
-    des: "This script allows you to draw on a live webcam screen using hand gestures, with the option to select different colors. It's a versatile tool that can be used in various real-life scenarios where you need to mark or inspect images, not necessarily on life camera.",
-    link: "https://github.com/navnoorsingh0309/Gesture-Drawing",
-    iconLists: ["/stack/python.png", "/stack/tensor-flow.png"]
+    title: "Fault Tolerant Control of UAV",
+    image: "/Projects/ideaforge.png",
+    description: "Single Motor Failure Recovery.",
+    link: "/",
   },
   {
-    id: "9",
-    title: "Lempel-Ziv-Welch Compression",
-    img: "/Projects/9.png",
-    des: "LZW is a compression algorithm that is table-based lookup algorithm to remove duplicate data and compress original file to a smaller size. This is the implemented of similar algorithm in C++;",
-    link: "https://github.com/navnoorsingh0309/Data_Compressiong_LZW",
-    iconLists: ["/stack/C.png"]
+    title: "Line Follower with PID Control",
+    image: "/Projects/line-follower.jpeg",
+    description: "Mobile bot that follows the line.",
+    link: "/",
   },
-  {
-    id: "10",
-    title: "Packet Sniffer",
-    img: "/Projects/10.jpg",
-    des: "This script sniffers packets through a network and inspects as they traverse a network. This script is made using kali linux and it will work on on linux OS. This can be used in real life to observe and manage network trafficking.",
-    link: "https://github.com/navnoorsingh0309/Packet-Sniffer",
-    iconLists: ["/stack/python.png"]
-  }
+  // {
+  //   // id: "1",
+  //   title: "Risc-V Assembler",
+  //   image: "/Projects/1.png",
+  //   description: "This is the implementation of a simple assembler which assembler Risc-V Assembly Code to Machine Code. The assembler supports various Risc-V instructions including R-type, I-Type, S-Type, SB-Type, U-Type, UJ-Type.",
+  //   link: "https://github.com/navnoorsingh0309/Riscv_Assembler",
+  //   // iconLists: ["/stack/C++.png"]
+  // },
+  // {
+  //   // id: "2",
+  //   title: "5 stage MIP32 Processor",
+  //   image: "/Projects/2.png",
+  //   description: "This is the implementation of a processor based on MIP32 Instruction Set Architecture(ISA) which can perform basic arithmetic operations and logical operations and can perform conditional branching. This processor can store and read data from a bank of registers using pipelining.",
+  //   link: "https://github.com/navnoorsingh0309/MIP32_Pipelined_Processor",
+  //   // iconLists: ["/stack/Verilog.png"]
+  // },
+  // {
+  //   id: "3",
+  //   title: "BOAA Website",
+  //   img: "/Projects/3.jpeg",
+  //   des: "I independently designed and developed the website for the Board of Academic Affairs at IIT Ropar using HTML, CSS, and Vanilla JavaScript. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quia recusandae nostrum id dolorem labore ipsum accusamus sequi magnam, optio praesentium inventore sapiente minima consequatur delectus temporibus dignissimos. Similique doloribus est commodi!",
+  //   link: "https://iitrpr.ac.in/boaa",
+  //   iconLists: ["/stack/html.png", "/stack/CSS.png", "/stack/js.png"]
+  // },
+  // {
+  //   id: "4",
+  //   title: "Comman Gateway Interface (CGI)",
+  //   img: "/Projects/4.png",
+  //   des: "The Common Gateway Interface (CGI) is a standard that facilitates communication between web servers and external databases or information sources. In this I have created a submission form which takes your information and one picture and then using CGI with Perl, it shows next page. I have create form in HTML with the help of Bootstrap. For php calling using POST method, I have utlized ajax in jquery, by which picture is saved on the server side, to be used by CGI to show client.",
+  //   link: "https://github.com/navnoorsingh0309/Common_Gateway_Interface",
+  //   iconLists: ["/stack/html.png", "/stack/CSS.png", "/stack/php.png", "/stack/perl.png"]
+  // },
+  // {
+  //   id: "5",
+  //   title: "Advanced Encryption Standard (AES)",
+  //   img: "/Projects/5.png",
+  //   des: "Advanced Encryption Standard (AES) is a specification for the encryption of electronic data established by the U.S National Institute of Standards and Technology (NIST) in 2001. This is a simple implementation of Advanced Encryption Standard(AES-128) to encrypt and decrypt 16 byte of sample data using C++.",
+  //   link: "https://github.com/navnoorsingh0309/Advanced_Encryption_Standard",
+  //   iconLists: ["/stack/C++.png"]
+  // },
+  // {
+  //   id: "6",
+  //   title: "TCP Client Server",
+  //   img: "/Projects/6.png",
+  //   des: "A Simple Chat Room which uses TCP service to convey messages and uses MongoDB for login and Sign Up purpose. It also utlitizes multithreading to support multiple requests at the same time. Sign Up features is also there to add new users and when one user left the chat, it automatically get deleted from server broadcasting channel.",
+  //   link: "https://github.com/navnoorsingh0309/TCP_Client_Server",
+  //   iconLists: ["/stack/python.png", "/stack/mongo.jpg"]
+  // },
+  // {
+  //   id: "7",
+  //   title: "Sentimental Analysis",
+  //   img: "/Projects/7.png",
+  //   des: "This project showcases a Sentimental Analysis Model that utilizes RNN and LSTM. The Model predicts your text is positive or negative along giving a score between 0 and 1. In this project first we clean data using various pre-processing techniques and then we create our model, structure is given in github.",
+  //   link: "https://github.com/navnoorsingh0309/Sentimental_Analysis_LSTM",
+  //   iconLists: ["/stack/python.png", "/stack/tensor-flow.png"]
+  // },
+  // {
+  //   id: "8",
+  //   title: "Gesture drawing using OpenCV",
+  //   img: "/Projects/8.png",
+  //   des: "This script allows you to draw on a live webcam screen using hand gestures, with the option to select different colors. It's a versatile tool that can be used in various real-life scenarios where you need to mark or inspect images, not necessarily on life camera.",
+  //   link: "https://github.com/navnoorsingh0309/Gesture-Drawing",
+  //   iconLists: ["/stack/python.png", "/stack/tensor-flow.png"]
+  // },
+  // {
+  //   id: "9",
+  //   title: "Lempel-Ziv-Welch Compression",
+  //   img: "/Projects/9.png",
+  //   des: "LZW is a compression algorithm that is table-based lookup algorithm to remove duplicate data and compress original file to a smaller size. This is the implemented of similar algorithm in C++;",
+  //   link: "https://github.com/navnoorsingh0309/Data_Compressiong_LZW",
+  //   iconLists: ["/stack/C.png"]
+  // },
+  // {
+  //   id: "10",
+  //   title: "Packet Sniffer",
+  //   image: "/Projects/10.jpg",
+  //   description: "This script sniffers packets through a network and inspects as they traverse a network. This script is made using kali linux and it will work on on linux OS. This can be used in real life to observe and manage network trafficking.",
+  //   link: "https://github.com/navnoorsingh0309/Packet-Sniffer",
+  //   iconLists: ["/stack/python.png"]
+  // }
 ];
 
 const Projects = () => {
   return (
-    <section className="flex flex-col justify-center space-y-4 items-center bg-blue-950/50 w-full p-4 px-[30px] sm:px-[70px] pt-[100px]" id="projects">
+    <section
+      className="flex flex-col justify-center space-y-4 items-center bg-blue-950/50 w-full p-4 px-[30px] sm:px-[70px] pt-[100px]"
+      id="projects"
+    >
       <h1
         className="text-4xl lg:text-7xl flex animate-bounce"
         style={{ fontFamily: "GaMaamli, sans-serif" }}
@@ -95,56 +152,7 @@ const Projects = () => {
         My &nbsp; <p className="text-[#ffd700]">Projects</p>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-y-32 lg:gap-y-40 gap-x-24 pt-10 sm:pt-20">
-        {projects.map((item) => (
-          <div
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-            key={item.id}
-          >
-            <PinContainer title="View" href={item.link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <a href={item.link} target="_blank">
-                <img src={item.img} alt="cover" className="z-10 w-full" />
-                </a>
-              </div>
-
-              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                {item.title}
-              </h1>
-
-              <p
-                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-6"
-                style={{
-                  color: "#BEC1DD",
-                  margin: "1vh 0",
-                }}
-              >
-                {item.des}
-              </p>
-
-              <div className="flex items-center justify-between mt-7 mb-3">
-                <div className="flex items-center">
-                  {item.iconLists.map((icon, index) => (
-                    <div
-                      key={index}
-                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                      style={{
-                        transform: `translateX(-${5 * index + 2}px)`,
-                      }}
-                    >
-                      <img src={icon} alt="icon5" className="p-2" />
-                    </div>
-                  ))}
-                </div>
-
-                <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Project Now
-                  </p>
-                </div>
-              </div>
-            </PinContainer>
-          </div>
-        ))}
+        <HoverEffect items={projects} />
       </div>
     </section>
   );
