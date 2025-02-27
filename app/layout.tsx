ccfc9d7c08dbc0177819c9fc8baa9497456cb91d
navnoorsingh0309 <nbbal2003@gmail.com>
2025-02-27 06:02:48 +0530

    Just some silly mistake, module not found

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-import { WavyBackground } from "@/components/wavy-background";
 import "./fonts.css";
 import { cn } from "@/lib/utils";
 import "./styles.css";
