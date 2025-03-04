4f2613b64c7e914a446e6959497c190ee25b327f
navnoorsingh0309 <nbbal2003@gmail.com>
2025-03-04 13:00:57 +0530

    Fixed sever client mismatch error

--- components/ui/AnimatedBackground.tsx
+++ components/ui/AnimatedBackground.tsx
@@ -1,37 +1,62 @@
-"use client"
+"use client";
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type Circle = {
+  xMove: number;
+  yMove: number;
+  top: number;
+  left: number;
+  duration:number;
+  delay:number;
+  size:number;
+}
 
 const AnimatedBackground = () => {
+  const [circles, setCircles] = useState<Circle[]>([]);
+
+  useEffect(() => {
+    const generatedCircles = Array.from({ length: 50 }).map(() => ({
+      left: Math.random() * 100,
+      top: Math.random() * 100,
+      size: Math.random() * 50 + 20,
+      delay: Math.random() * 2,
+      duration: Math.random() * 3 + 2,
+      xMove: Math.random() * 100 - 50,
+      yMove: Math.random() * 100 - 50,
+    }));
+    setCircles(generatedCircles);
+  }, []);
+
   return (
-    <div className="fixed inset-0 overflow-hidden">
+    <div className="fixed inset-0 overflow-hidden -z-50">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 0.5 }}
         transition={{ duration: 1 }}
         className="absolute inset-0"
       >
-        {Array.from({ length: 50 }).map((_, i) => (
+        {circles.map((circle, i) => (
           <motion.div
             key={i}
             initial={{ opacity: 0, scale: 0 }}
             animate={{
               opacity: [0.5, 0.7, 0.5],
               scale: [1, 1.2, 1],
-              x: [0, Math.random() * 100 - 50, 0],
-              y: [0, Math.random() * 100 - 50, 0],
+              x: [0, circle.xMove, 0],
+              y: [0, circle.yMove, 0],
             }}
             transition={{
-              duration: Math.random() * 3 + 2,
+              duration: circle.duration,
               repeat: Number.POSITIVE_INFINITY,
-              delay: Math.random() * 2,
+              delay: circle.delay,
             }}
             className="absolute rounded-full bg-primary/10"
             style={{
-              left: `${Math.random() * 100}%`,
-              top: `${Math.random() * 100}%`,
-              width: `${Math.random() * 50 + 20}px`,
-              height: `${Math.random() * 50 + 20}px`,
+              left: `${circle.left}%`,
+              top: `${circle.top}%`,
+              width: `${circle.size}px`,
+              height: `${circle.size}px`,
             }}
           />
         ))}
